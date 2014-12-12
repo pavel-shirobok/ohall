21c3b79e67b72e23ec3cbac2c1b99ed958c21363
Shirobok Pavel <deeptrader@yandex.ru>
2014-12-12 22:02:48 +0800

    some commit

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var request = require('request');
 var info = require('./package_infos.js');
 var _ = require('lodash');
 var zip = require('node-zip');
+var fs = require('fs');
 
 var CDN_ROOT = 'http://localhost:333';
 
@@ -68,11 +69,28 @@ function Dummer() {
 
                 var unpacked = zip(data, {base64:false, checkCRC32:true });
 
-                _.each(unpacked.files['js/jquery-1.11.1.js'], function(key, v){
-                    console.log(v, key._data);
+                _.each(unpacked.files, function(value, key){
+                    if(key.charAt(key.length - 1) == '/'){
+                        if (!fs.existsSync(key)){
+                            fs.mkdirSync(key);
+                        }
+                        return
+                    }
+
+                    var path = dest + '/' + key;
+                    //console.log(path);
+
+                    fs.writeFile(key, value._data, function(err){
+                        console.log(path, err == undefined);
+                    })
+
+                    //console.log(v, key._data);
                 });
 
-                console.log(unpacked.files['js/jquery-1.11.1.js']._data.length);
+
+
+
+                //console.log(unpacked.files['js/jquery-1.11.1.js']._data.length);
 
                 onFinish && onFinish();
         },function(err){
