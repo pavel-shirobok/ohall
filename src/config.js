e01c5d7e7c480690f8b5aa0057258aca454c5c96
Shirobok Pavel <deeptrader@yandex.ru>
2014-12-29 02:02:40 +0800

    Setting as repository default is git-source

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ var config = {};
 config.PACKAGES_FILENAME = 'packages.json';
 config.GLOBAL_CONFIG_FILENAME = '.ohallc';
 config.DEFAULT_GLOBAL_CONFIG = {
-    CDN_URL : 'http://localhost:33'
+    CDN_URL : 'https://raw.githubusercontent.com/ramshteks/ohall-repository/master/'
 };
 
 module.exports = config;
